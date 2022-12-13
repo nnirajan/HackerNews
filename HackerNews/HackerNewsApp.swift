b48bfb0ff37447f96e804c59703856ff35db5660
Nirajan Shrestha <nirajan.shrestha@ebpearls.com>
2022-12-13 10:46:57 +0545

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by ebpearls on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
